e53587c138657f299be8ba99b3ad5aea8b9a9c24
Aastha Yadav <yadavaastha00@gmail.com>
2026-02-10 18:25:29 +0530

    Update rag.py

--- ai code reviewer/rag.py
+++ ai code reviewer/rag.py
@@ -2,18 +2,23 @@ from langchain_community.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 import os
 
 
 def create_vector_db():
 
-    files = [
-        "rag_data/clean_code.txt",
-        "rag_data/solid.txt",
-        "rag_data/security.txt",
-        "rag_data/performance.txt"
-    ]
+  files = [
+    os.path.join(BASE_DIR, "rag_data/clean_code.txt"),
+    os.path.join(BASE_DIR, "rag_data/solid.txt"),
+    os.path.join(BASE_DIR, "rag_data/security.txt"),
+    os.path.join(BASE_DIR, "rag_data/performance.txt")
+]
+
 
     docs = []
 
@@ -56,3 +61,4 @@ def get_relevant_knowledge(query):
         context += d.page_content + "\n"
 
     return context
+
