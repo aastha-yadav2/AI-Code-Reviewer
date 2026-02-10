e0379873653cfdae8de7b4c0e4a0cfae7fa6af31
Aastha Yadav <yadavaastha00@gmail.com>
2026-02-10 18:44:21 +0530

    Update rag.py

--- ai code reviewer/rag.py
+++ ai code reviewer/rag.py
@@ -4,21 +4,18 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 import os
 
+# ----- BASE PATH -----
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-import os
 
 
 def create_vector_db():
 
-  files = [
-    os.path.join(BASE_DIR, "rag_data/clean_code.txt"),
-    os.path.join(BASE_DIR, "rag_data/solid.txt"),
-    os.path.join(BASE_DIR, "rag_data/security.txt"),
-    os.path.join(BASE_DIR, "rag_data/performance.txt")
-]
-
+    files = [
+        os.path.join(BASE_DIR, "rag_data/clean_code.txt"),
+        os.path.join(BASE_DIR, "rag_data/solid.txt"),
+        os.path.join(BASE_DIR, "rag_data/security.txt"),
+        os.path.join(BASE_DIR, "rag_data/performance.txt")
+    ]
 
     docs = []
 
@@ -29,15 +26,15 @@ def create_vector_db():
     splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=20)
     split_docs = splitter.split_documents(docs)
 
-    embeddings = HuggingFaceEmbeddings()
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
 
     db = Chroma.from_documents(
         split_docs,
         embeddings,
-        persist_directory="db"
+        persist_directory=os.path.join(BASE_DIR, "db")
     )
-
-    
 
     return db
 
@@ -45,11 +42,12 @@ def create_vector_db():
 
 def get_relevant_knowledge(query):
 
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
-
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-mpnet-base-v2"
+    )
 
     db = Chroma(
-        persist_directory="db",
+        persist_directory=os.path.join(BASE_DIR, "db"),
         embedding_function=embeddings
     )
 
@@ -61,4 +59,3 @@ def get_relevant_knowledge(query):
         context += d.page_content + "\n"
 
     return context
-
